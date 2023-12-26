d451ac2ae08acb8170d1fe91fefb658f1eb5be15
Dev <devendra08440@gmail.com>
2023-12-26 16:02:38 +0530

    Component Home

--- code/src/App.js
+++ code/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Registration from './Components/Registration/Registation';
+import Home from './Components/Registration/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <Home/>
       <Routes>
         <Route path='/Registration' element={<Registration/>}/>
       </Routes>
