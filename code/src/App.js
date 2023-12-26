9427b277066104d82aa2a54fcdfffd93ade045d1
Dev <devendra08440@gmail.com>
2023-12-26 15:54:37 +0530

    Adding Routes

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Registration from './Components/Registration/Registation';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/Registation' element={<Registration/>}/>
+      </Routes>
     </div>
   );
 }
